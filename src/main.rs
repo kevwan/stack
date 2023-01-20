1a6d415ec15207d870bfc4a00178f0b15dc1b138
kevin <wanjunfeng@gmail.com>
2023-01-20 20:27:47 +0800

    feat: process stdin stream

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use arboard::Clipboard;
 use std::env;
-use std::io::prelude::*;
-use std::str;
+use std::io::{self, BufRead};
 
 fn main() {
     let mut args = env::args();
@@ -15,28 +14,14 @@ fn main() {
 fn handle_clipboard() {
     let input = read_clipboard();
     let output = process_input(input);
-    println!("\n{}", output);
+    println!("{}", output);
 }
 
 fn handle_stdin() {
-    let mut buffer = Vec::with_capacity(256);
-    let stdin = std::io::stdin();
-    let mut handle = stdin.lock();
-    match handle.read_to_end(&mut buffer) {
-        Ok(_) => (),
-        Err(e) => {
-            println!("error: {}", e);
-        }
-    }
-
-    let stdout = std::io::stdout();
-    let mut handle = stdout.lock();
-
-    match handle.write_all(&{ process_vec(&buffer) }) {
-        Ok(_) => (),
-        Err(e) => {
-            println!("error: {}", e);
-        }
+    for line in io::stdin().lock().lines() {
+        let input = line.unwrap();
+        let output = process_input(input);
+        println!("{}", output);
     }
 }
 
@@ -45,14 +30,6 @@ fn process_input(input: String) -> String {
         .replace("\\r", "\r")
         .replace("\\n", "\n")
         .replace("\\t", "\t")
-}
-
-fn process_vec(input: &Vec<u8>) -> Vec<u8> {
-    let s = match str::from_utf8(input) {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
-    process_input(s.to_string()).into_bytes()
 }
 
 fn read_clipboard() -> String {
